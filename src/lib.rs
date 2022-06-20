b23acaa6a29f178045e5c95d8a57573ecb961bd3
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-06-20 23:06:38 +0200

    [ERASE] - Erase entity creation
    [+] Create Erase entity

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+// Librairie use in this librairie
+
+// Erase entity
+#[derive(Debug, Clone, Copy)]
+pub enum EraserEntity {
+    Dod522022MECE,
+    Dod522022ME,
+    DoD522028MSTD,
+    UsArmyAr38019,
+    Afssi5020,
+    RcmpTssitOpsII,
+    HmgiS5,
+    Gutmann
+}
