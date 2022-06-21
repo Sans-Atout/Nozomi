7c8b7b731f22253b6902b46ff4d9feea70bfc12e
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-06-21 19:10:40 +0200

    [ERASE] - Overwritting function
    [+] Creation of a function to write over a file to actually delete its contents

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,20 @@ pub enum EraserEntity {
     HmgiS5,
     Gutmann
 }
+
+fn file_overwriting(_path : &str, _char : [u8; 3])-> io::Result<()> {
+    // Declare important variable for this 
+    let mut _file = File::options().read(true).open(_path)?;
+    let mut reader = BufReader::new(_file);
+    let mut buffer = Vec::new();
+    let mut buffer_modified = Vec::new();
+    
+    reader.read_to_end(&mut buffer)?;
+    for _id in 0..buffer.len() { buffer_modified.push(_char[_id % 3]);}
+
+     _file = File::options().write(true).open(_path)?;
+    _file.write_all(buffer_modified.as_slice())?;
+
+    Ok(())
+}
+
