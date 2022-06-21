dc8410f5b6f683ceb25606456b485e722cca32af
Augustin ROUSSET-ROUVIERE <augustin.rousset-rouviere@isep.fr>
2022-06-21 22:35:08 +0200

    [ERASE] - Adding two erase method
    [+] Add gutmann erase method
    [+] Add hmgi s5 erase method

--- src/lib.rs
+++ src/lib.rs
@@ -53,3 +53,67 @@ fn file_overwriting_random(_path : &str)-> io::Result<()> {
 
     Ok(())
 }
+
+fn gutmann_overwrite_file(_path : &str)-> io::Result<()> {
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    file_overwriting(_path,[0x55 as u8,0x55 as u8,0x55 as u8])?;
+    file_overwriting(_path,[0xAA as u8,0xAA as u8,0xAA as u8])?;
+    file_overwriting(_path,[0x92 as u8,0x49 as u8,0x24 as u8])?;
+    file_overwriting(_path,[0x00 as u8,0x00 as u8,0x00 as u8])?;
+    file_overwriting(_path,[0x11 as u8,0x11 as u8,0x11 as u8])?;
+    file_overwriting(_path,[0x22 as u8,0x22 as u8,0x22 as u8])?;
+    file_overwriting(_path,[0x33 as u8,0x33 as u8,0x33 as u8])?;
+    file_overwriting(_path,[0x44 as u8,0x44 as u8,0x44 as u8])?;
+    file_overwriting(_path,[0x55 as u8,0x55 as u8,0x55 as u8])?;
+    file_overwriting(_path,[0x66 as u8,0x66 as u8,0x66 as u8])?;
+    file_overwriting(_path,[0x77 as u8,0x77 as u8,0x77 as u8])?;
+    file_overwriting(_path,[0x88 as u8,0x88 as u8,0x88 as u8])?;
+    file_overwriting(_path,[0x99 as u8,0x99 as u8,0x99 as u8])?;
+    file_overwriting(_path,[0xAA as u8,0xAA as u8,0xAA as u8])?;
+    file_overwriting(_path,[0xBB as u8,0xBB as u8,0xBB as u8])?;
+    file_overwriting(_path,[0xCC as u8,0xCC as u8,0xCC as u8])?;
+    file_overwriting(_path,[0xDD as u8,0xDD as u8,0xDD as u8])?;
+    file_overwriting(_path,[0xEE as u8,0xEE as u8,0xEE as u8])?;
+    file_overwriting(_path,[0xFF as u8,0xFF as u8,0xFF as u8])?;
+    file_overwriting(_path,[0x92 as u8,0x49 as u8,0x24 as u8])?;
+    file_overwriting(_path,[0x49 as u8,0x24 as u8,0x92 as u8])?;
+    file_overwriting(_path,[0x6D as u8,0xB6 as u8,0xDB as u8])?;
+    file_overwriting(_path,[0xB6 as u8,0xDB as u8,0x6D as u8])?;
+    file_overwriting(_path,[0xDB as u8,0x6D as u8,0xB6 as u8])?;
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    file_overwriting_random(_path)?;
+    Ok(())
+}
+
+fn hmgi_s5_overwrite_file(_path : &str)-> Result<bool, &'static str >{
+    match file_overwriting(_path,[0x00 as u8,0x00 as u8,0x00 as u8]){
+        Ok(_) => true,
+        Err(_) => return Err("Error in the first pass")
+    };
+    match file_overwriting(_path,[0xFF as u8,0xFF as u8,0xFF as u8]){
+        Ok(_) => true,
+        Err(_) => return Err("Error in the second pass")
+    };
+    let mut _file = match File::options().read(true).open(_path){
+        Ok(file) => file,
+        Err(_) => return Err("Error in file opening")
+    };
+
+    let mut reader = BufReader::new(_file);
+    let mut buffer = Vec::new();
+    let _d = match reader.read_to_end(&mut buffer){
+        Ok(_) => true,
+        Err(_) => return Err("Error in buffer reading")
+    };
+    for _char in buffer { 
+        if ! _char as u8 == 0xFF as u8{
+            return Err("One of byte is not equal at 0xFF")
+        }
+    }
+    return Ok(true)
+}
